7ee18d5a4e435664677047fbe88a5bff1f4ec83d
Chris Hannaby <chris.hannaby@netlify.com>
2021-11-29 11:50:07 -0700

    exit preview

--- components/PreviewBanner/index.js
+++ components/PreviewBanner/index.js
@@ -4,6 +4,7 @@ export default function PreviewBanner() {
   return (
     <div className={PreviewBannerStyles.preview}>
       <p className={PreviewBannerStyles.preview__text}>Preview Mode</p>
+      <a href="/api/endpreview">Exit Preview</a>
     </div>
   );
 }
