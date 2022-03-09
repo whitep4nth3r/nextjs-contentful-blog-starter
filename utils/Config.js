970c1e351df69625be5e3083e382219235dde6ec
Vaclavklicka <vasek@klicka.eu>
2022-03-09 23:09:16 +0100

    init

--- utils/Config.js
+++ utils/Config.js
@@ -13,7 +13,7 @@ const SITE_URL = "https://nextjs-contentful-blog-starter.vercel.app";
 
 export const Config = {
   site: {
-    owner: "VK",
+    owner: "VK test",
     title: "My new Next.js + Contentful blog site",
     domain: "nextjs-contentful-blog-starter.vercel.app",
     email: "example@example.com",
